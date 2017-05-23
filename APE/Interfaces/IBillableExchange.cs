4ab864759029e216a22c1575c177cb846102ee3d
alien <NAlex2004@gmail.com>
2017-05-23 09:00:45 +0300

    .

---
+++ APE/Interfaces/IBillableExchange.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using NAlex.APE.Event;
+
+namespace NAlex.APE
+{
+	public interface IBillableExchange
+	{
+		IEnumerable<CallEventArgs> CallsLog { get; }
+		event CallEventHandler CallPermissionCheck;
+	}
+}
--- APE/PhoneExchange.cs
+++ APE/PhoneExchange.cs
@@ -7,148 +7,167 @@ using NAlex.APE.Interfaces;
 
 namespace NAlex.APE
 {
-    public class PhoneExchange : IPhoneExchange
-    {
-        private IList<IPort> _ports = new List<IPort>();
-        private IList<CallEventArgs> _callLog = new List<CallEventArgs>();
-        private IPortId _portId;
+	public class PhoneExchange : IPhoneExchange, IBillableExchange
+	{
+		private IList<IPort> _ports = new List<IPort>();
+		private IList<CallEventArgs> _callLog = new List<CallEventArgs>();
+		private IPortId _portId;
 
-        public event CallEventHandler CallStarted;
-        public event CallEventHandler CallEnded;
-        public event CallEventHandler CallAccepted;
-        public event PortStateEventHandler PortAdded;
-        public event PortStateEventHandler PortRemoved;
+		public event CallEventHandler CallStarted;
+		public event CallEventHandler CallEnded;
+		public event CallEventHandler CallAccepted;
+		public event PortStateEventHandler PortAdded;
+		public event PortStateEventHandler PortRemoved;
+		public event CallEventHandler CallPermissionCheck;
 
-        public IEnumerable<IPort> Ports
-        {
-            get { return _ports; }
-        }
+		public IEnumerable<IPort> Ports
+		{
+			get { return _ports; }
+		}
 
-        public IEnumerable<CallEventArgs> CallsLog
-        {
-            get { return _callLog; }
-        }
+		public IEnumerable<CallEventArgs> CallsLog
+		{
+			get { return _callLog; }
+		}
 
-        public PhoneExchange(IPortId startPortId)
-        {
-            _portId = startPortId;
-        }
+		public PhoneExchange(IPortId startPortId)
+		{
+			_portId = startPortId;
+		}
 
-        public IPort CreatePort(IPortFactory portFactory)
-        {
-            // На события станции порт подпишится сам при событии PortAdded
-            // А на него - в конструкторе
-            IPort port = portFactory.CreatePort(_portId);
-            _portId = _portId.NextValue();
-            port.ApeCallStarted += PortCallStarted;
-            port.ApeCallEnded += PortCallEnded;
-            port.ApeCallAccepted += PortCallAccepted;
-            _ports.Add(port);
-            OnPortAdded(port);
-            return port;
-        }
+		public IPort CreatePort(IPortFactory portFactory)
+		{
+			// На события станции порт подпишится сам при событии PortAdded
+			// А на него - в конструкторе
+			IPort port = portFactory.CreatePort(_portId);
+			_portId = _portId.NextValue();
+			port.ApeCallStarted += PortCallStarted;
+			port.ApeCallEnded += PortCallEnded;
+			port.ApeCallAccepted += PortCallAccepted;
+			_ports.Add(port);
+			OnPortAdded(port);
+			return port;
+		}
 
-        protected virtual void OnPortAdded(IPort port)
-        {
-            if (PortAdded != null)
-                PortAdded(this, new PortEventArgs() {Port = port});
-        }
+		protected virtual void OnPortAdded(IPort port)
+		{
+			if (PortAdded != null)
+				PortAdded(this, new PortEventArgs() { Port = port });
+		}
 
-        protected virtual void OnPortRemoved(IPort port)
-        {
-            if (PortRemoved != null)
-                PortRemoved(this, new PortEventArgs() {Port = port});
-        }
+		protected virtual void OnPortRemoved(IPort port)
+		{
+			if (PortRemoved != null)
+				PortRemoved(this, new PortEventArgs() { Port = port });
+		}
 
-        protected virtual void OnCall(CallEventHandler handler, CallEventArgs e, IPortId destPortId)
-        {
-            Debug.WriteLine("[PhoneExchange.OnCall]");
-            Debug.WriteLine(e);
-            
-            if (handler != null)
-            {
-                // Только один нужный порт может быть
-                var destPortHandler = handler.GetInvocationList()
-                    .Where(d => (d.Target as IPort) != null && ((IPort) d.Target).PortId.Equals(destPortId))
-                    .OfType<CallEventHandler>()
-                    .FirstOrDefault();
+		protected virtual void OnCall(CallEventHandler handler, CallEventArgs e, IPortId destPortId)
+		{
+			Debug.WriteLine("[PhoneExchange.OnCall]");
+			Debug.WriteLine(e);
 
-                var invListOthers = handler.GetInvocationList().Where(d => !(d.Target is IPort))
-                    .OfType<CallEventHandler>();
+			if (handler != null)
+			{
+				// Только один нужный порт может быть
+				var destPortHandler = handler.GetInvocationList()
+					.Where(d => (d.Target as IPort) != null && ((IPort)d.Target).PortId.Equals(destPortId))
+					.OfType<CallEventHandler>()
+					.FirstOrDefault();
 
-                // Вызвать событие только на порту назначения и для всех подписчиков - не портов
-                if (destPortHandler != null)
-                    destPortHandler(this, e);
-                else
-                    e.State = CallEventStates.Invalid;
+				var invListOthers = handler.GetInvocationList().Where(d => !(d.Target is IPort))
+					.OfType<CallEventHandler>();
 
-                _callLog.Add(e);
+				// Вызвать событие только на порту назначения и для всех подписчиков - не портов
+				if (destPortHandler != null)
+					destPortHandler(this, e);
+				else
+					e.State = CallEventStates.Invalid;
 
-                foreach (var callEventHandler in invListOthers)
-                {
-                    callEventHandler(this, e);
-                }
-            }
-        }
+				_callLog.Add(e);
 
-        // Для портов
-        protected virtual void OnCallStarted(CallEventArgs e)
-        {
-//            e.State = CallEventStates.Started;
-            OnCall(CallStarted, e, e.DestinationPortId);
-        }
+				foreach (var callEventHandler in invListOthers)
+				{
+					callEventHandler(this, e);
+				}
+			}
+		}
 
-        protected virtual void OnCallEnded(CallEventArgs e)
-        {
-            OnCall(CallEnded, e, e.DestinationPortId);
-        }
+		// Для портов
+		protected virtual void OnCallStarted(CallEventArgs e)
+		{
+			//            e.State = CallEventStates.Started;
+			OnCall(CallStarted, e, e.DestinationPortId);
+		}
 
-        protected virtual void OnCallAccepted(CallEventArgs e)
-        {
-            OnCall(CallAccepted, e, e.SourcePortId);
-        }
-        
-        // Подписки на события от портов
-        protected virtual void PortCallStarted(object sender, CallEventArgs e)
-        {
-            var destPort = _ports.FirstOrDefault(p => p.PortId.Equals(e.DestinationPortId));
-            if (destPort != null)
-            {
-                if (destPort.PortState == PortStates.Connected)
-                    OnCallStarted(e);
-                else
-                {
-                    e.State = CallEventStates.Missed;
-                    OnCall(CallEnded, e, e.SourcePortId);
-                }
-            }
-            else
-            {
-                // порт назначения не найден, ответ посылается источнику
-                e.State = CallEventStates.Invalid;
-                OnCall(CallEnded, e, e.SourcePortId);
-            }
-        }
+		protected virtual void OnCallEnded(CallEventArgs e)
+		{
+			OnCall(CallEnded, e, e.DestinationPortId);
+		}
 
-        protected virtual void PortCallEnded(object sender, CallEventArgs e)
-        {
-            // разделить входящий и исходящий звонок
-            IPortId destPortId = e.State == CallEventStates.IncommingCallFinished
-                ? e.SourcePortId
-                : e.DestinationPortId;            
-            
-            var destPort = _ports.FirstOrDefault(p => p.PortId.Equals(destPortId));
-            if (destPort != null)
-            {
+		protected virtual void OnCallAccepted(CallEventArgs e)
+		{
+			OnCall(CallAccepted, e, e.SourcePortId);
+		}
+
+		protected virtual void OnCallPermissionCheck(CallEventArgs e)
+		{
+			if (CallPermissionCheck != null)
+			{
+				CallPermissionCheck(this, e);
+			}
+			else
+				e.IsAllowed = true;
+		}
+
+		// Подписки на события от портов
+		protected virtual void PortCallStarted(object sender, CallEventArgs e)
+		{
+			OnCallPermissionCheck(e);
+			if (!e.IsAllowed)
+			{
+				e.State = CallEventStates.Denied;
+				OnCall(CallEnded, e, e.SourcePortId);
+				return;
+			}
+
+			var destPort = _ports.FirstOrDefault(p => p.PortId.Equals(e.DestinationPortId));
+			if (destPort != null)
+			{
+				if (destPort.PortState == PortStates.Connected)
+					OnCallStarted(e);
+				else
+				{
+					e.State = CallEventStates.Missed;
+					OnCall(CallEnded, e, e.SourcePortId);
+				}
+			}
+			else
+			{
+				// порт назначения не найден, ответ посылается источнику
+				e.State = CallEventStates.Invalid;
+				OnCall(CallEnded, e, e.SourcePortId);
+			}
+		}
+
+		protected virtual void PortCallEnded(object sender, CallEventArgs e)
+		{
+			// разделить входящий и исходящий звонок
+			IPortId destPortId = e.State == CallEventStates.IncommingCallFinished
+				? e.SourcePortId
+				: e.DestinationPortId;
+
+			var destPort = _ports.FirstOrDefault(p => p.PortId.Equals(destPortId));
+			if (destPort != null)
+			{
 				OnCall(CallEnded, e, destPortId);
-            }
-        }
+			}
+		}
 
-        protected virtual void PortCallAccepted(object sender, CallEventArgs e)
-        {
-            var destPort = _ports.FirstOrDefault(p => p.PortId.Equals(e.SourcePortId));
-            if (destPort != null)
-                OnCallAccepted(e);
-        }
-    }
+		protected virtual void PortCallAccepted(object sender, CallEventArgs e)
+		{
+			var destPort = _ports.FirstOrDefault(p => p.PortId.Equals(e.SourcePortId));
+			if (destPort != null)
+				OnCallAccepted(e);
+		}
+	}
 }
